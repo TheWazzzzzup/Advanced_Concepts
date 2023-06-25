70f8b111aeebc337a561c7972c286fab7ed32158
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-06-25 12:35:00 +0200

    Goal Achevied
    created the UV to make the texture spread accordingly on the mesh

--- Assets/Scirpts(AmitPG)/PerlinNoise.cs
+++ Assets/Scirpts(AmitPG)/PerlinNoise.cs
@@ -22,8 +22,9 @@ public class PerlinNoise : MonoBehaviour
     {
         noiseMap = CreateNoiseMap();
         perlinRenderer.sharedMaterial.mainTexture = DrawNoiseMap(xyLength);
-        transform.localScale = new Vector3(xyLength, 1, xyLength);
         mesh.mesh = MeshGen.GenerateMeshData(noiseMap);
+        transform.localScale = new Vector3(1, 8, 1);
+        mesh.mesh.RecalculateNormals();
     }
 
     private void OnValidate()
@@ -82,19 +83,20 @@ public static class MeshGen
 
         Vector3[] verts = new Vector3[width*heigth];
         int[] triangles = new int[(width - 1) * (heigth - 1) * 6];
+        Vector2[] uv = new Vector2[width*heigth]; 
+        
 
         int vertRowLength = width - 1;
 
         int vertexIndex = 0;
         int triangleIndex = 0;
 
-        float lerpPos;
-
         for (int y = 0; y < heigth; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                verts[vertexIndex] = new Vector3(1 * x, Mathf.Lerp(0, 1000, noiseMap[x,y]), 1 * -y) ;
+                verts[vertexIndex] = new Vector3(1 * x, Mathf.Lerp(0, 1, noiseMap[x,y]), 1 * -y) ;
+                uv[vertexIndex] = new Vector2(x / (float)width, y / (float)heigth);
 
                 if (y < heigth - 1 && x < width - 1)
                 {
@@ -111,9 +113,12 @@ public static class MeshGen
                 vertexIndex++;
             }
         }
+
         Mesh mesh = new Mesh();
         mesh.vertices = verts;
         mesh.triangles = triangles;
+        mesh.uv = uv;
+        mesh.RecalculateNormals();
 
         return mesh;
     }
